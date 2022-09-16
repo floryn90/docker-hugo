cf174db46d31900fc7739d6c08b980395e2ab074
Florin Lungu <info@lunguflorin.ro>
2022-09-16 10:55:57 +0300

    bugfixes

--- src/files/_script/hugo-extended.sh
+++ src/files/_script/hugo-extended.sh
@@ -12,32 +12,35 @@ TARGETPLATFORM=${TARGETPLATFORM:-linux/amd64}
 if [ "$TARGETPLATFORM" = "linux/amd64" ]; then
     HUGO_ARCH="64bit"
     HUGOGURU_ARCH="x86_64"
+    HUGO_FILE="hugo_extended_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz"
 elif [ "$TARGETPLATFORM" = "linux/arm64" ]; then
-    HUGO_ARCH="ARM64"
+    HUGO_ARCH="arm64"
     HUGOGURU_ARCH="aarch64"
+    HUGO_FILE="hugo_extended_${HUGO_VERSION}_linux-${HUGO_ARCH}.tar.gz"
 elif [ "$TARGETPLATFORM" = "linux/arm/v7" ]; then
-    HUGO_ARCH="ARM"
+    HUGO_ARCH="arm"
     HUGOGURU_ARCH="armv7l"
+    HUGO_FILE="hugo_extended_${HUGO_VERSION}_linux-${HUGO_ARCH}.tar.gz"
 else
     echo "Unknown build architecture: $TARGETPLATFORM"
     exit 2
 fi
 
 # Download binaries from release
-wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz
+wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_FILE}
 wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_checksums.txt
 #wget https://github.com/hugoguru/dist-hugo/releases/download/v${HUGO_VERSION}/hugo-extended-${HUGO_VERSION}-linux-${HUGOGURU_ARCH}.tar.gz
 #wget https://github.com/hugoguru/dist-hugo/releases/download/v${HUGO_VERSION}/hugo-${HUGO_VERSION}-checksums-sha256.txt
 
 # Verify checksums
-grep -i hugo_extended_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz hugo_${HUGO_VERSION}_checksums.txt | sha256sum -c
+grep  ${HUGO_FILE} hugo_${HUGO_VERSION}_checksums.txt | sha256sum -c
 #grep hugo-extended-${HUGO_VERSION}-linux-${HUGOGURU_ARCH}.tar.gz hugo-${HUGO_VERSION}-checksums-sha256.txt | sha256sum -c
 
 # Prepare folders
 mkdir -p /files/usr/lib/hugo
 
 # Unpack downloaded content
-tar -zxf hugo_extended_${HUGO_VERSION}_Linux-${HUGO_ARCH}.tar.gz -C /files/usr/lib/hugo
+tar -zxf ${HUGO_FILE} -C /files/usr/lib/hugo
 #tar -zxf hugo-extended-${HUGO_VERSION}-linux-${HUGOGURU_ARCH}.tar.gz -C /files/usr/lib/hugo
 
 # Verify executable
